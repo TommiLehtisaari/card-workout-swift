9c94d5e1ece034104584b99838e9b0bdc2a20081
Tommi Lehtisaari <tommi.lehtisaari@fraktio.fi>
2020-11-27 15:26:25 +0200

    Initial Commit

---
+++ card-workout-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  card-workout-swift
+//
+//  Created by Tommi Lehtisaari on 27.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
